f35d15553cc2c234fa1910a8cd6c8b620920cff8
AKPR2007 <88362716+AKPR2007@users.noreply.github.com>
2022-04-21 22:04:50 +0600

    Update centos.sh

--- install/centos.sh
+++ install/centos.sh
@@ -33,7 +33,7 @@ if [ "$first" != 1 ]; then
 			exit 1
 			;;
 		esac
-		wget "https://raw.githubusercontent.com/AKPR2007/AarLinux/main/rootfs/centos/centos-rootfs-${archurl}.tar.xz" -O $tarball
+		wget "https://raw.githubusercontent.com/AKPR2007/termux-linux/main/rootfs/centos/centos-rootfs-${archurl}.tar.xz" -O $tarball
 	fi
 	cur=$(pwd)
 	mkdir -p "$folder"
